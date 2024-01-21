b4b88b09ae4b35b9b6a1b4f6dfc7b8791cf1dec4
Sangeeth Nandakumar <twileloop@outlook.com>
2024-01-21 15:43:41 +0530

    Add support for singleton based LiteDB implementation

--- Twileloop.UOW.LiteDB/Support/Extensions.cs
+++ Twileloop.UOW.LiteDB/Support/Extensions.cs
@@ -9,6 +9,22 @@ namespace Twileloop.UOW.LiteDB.Support
 
     public static class Extensions
     {
+        public static void AddSingletonUnitOfWork(this IServiceCollection services, Action<Options>? options = null)
+        {
+            var uowOptions = new Options();
+            options?.Invoke(uowOptions);
+
+            var contexts = new ConcurrentDictionary<string, LiteDBContext>();
+            foreach (var conn in uowOptions.Connections)
+            {
+                var db = new LiteDatabase(conn.ConnectionString);
+                contexts.TryAdd(conn.Name, new LiteDBContext(conn.Name, db));
+            }
+
+            services.AddSingleton(contexts);
+            services.AddSingleton<UnitOfWork>();
+        }
+
         public static void AddUnitOfWork(this IServiceCollection services, Action<Options>? options = null)
         {
             var uowOptions = new Options();
